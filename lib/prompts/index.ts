d63388633610d4c5f0767ecdf44a98428053e774
yohan.choi <yohanchoi.work@gmail.com>
2026-04-01 14:40:21 +0900

    feat : separate logic(GEN docs, format)

---
+++ lib/prompts/index.ts
@@ -0,0 +1,3 @@
+export { CODE_GENERATION_SYSTEM_RULES } from './code-generation-rules';
+export { FILE_FORMAT_INSTRUCTIONS } from './file-format';
+export { SURGICAL_FILE_EDIT_SYSTEM_RULES } from './surgical-edit-rules';
